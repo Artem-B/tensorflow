7278c3d9ca5ad800d9c18746a7d84e9a764d4c0c
Artem Belevich <tra@google.com>
2019-12-20 23:33:13 +0000

    TF lite fixes.

--- tensorflow/lite/kernels/internal/optimized/neon_tensor_utils.cc
+++ tensorflow/lite/kernels/internal/optimized/neon_tensor_utils.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 #include <sys/types.h>
-#include <unistd.h>
+//#include <unistd.h>
 
 #include <algorithm>
 #include <cmath>
